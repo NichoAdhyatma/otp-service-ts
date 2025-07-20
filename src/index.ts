f9b82c7f7b6c3bcdf44570824beded8106cc3405
NichoAdhyatma <nicholausadhyatma@gmail.com>
2025-07-20 15:40:16 +0700

    support serverless

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+// index.ts
 import dotenv from "dotenv";
 import app from "./app";
+import serverless from "serverless-http";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 OTP Service running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`🚀 OTP Service running on http://localhost:${PORT}`);
+  });
+}
 
+export default serverless(app);
