6974dae78682829334cdf63a682070ca23abbc0c
NichoAdhyatma <nicholausadhyatma@gmail.com>
2025-07-24 10:39:06 +0700

    update vercel

--- src/app.ts
+++ src/app.ts
@@ -6,16 +6,11 @@ import otpRouter from "./presentation/routes/otp-routes";
 import baseRoute from "./presentation/routes/base-routes";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(requestLogger);
 app.use(limiter);
-
-app.use((req, res, next) => {
-  console.log(`[DEBUG] ${req.method} ${req.url}`);
-  next();
-});
-
 
 app.use("/api", otpRouter);
 
